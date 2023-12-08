4a5a33a4d97317ea0a50088e61cfef381866bd04
CrashTagiyev <121178020+CrashTagiyev@users.noreply.github.com>
2023-12-08 17:32:58 +0400

    Add project files.

---
+++ ListenerConsoleApp/UdpServer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Windows.Forms;
+
+await UpUDPServer();
+async Task UpUDPServer()
+{
+    var server = new UdpClient(27001);
+    var remoteEP = new IPEndPoint(IPAddress.Loopback, 0);
+    server.Connect(remoteEP);
+    string command;
+
+    var client = new UdpClient();
+    var clientEP = new IPEndPoint(IPAddress.Loopback, 27002);
+
+    while (true)
+    {
+        var bytes = await server.ReceiveAsync();
+        command = Encoding.Default.GetString(bytes.Buffer);
+        if (command != null)
+        {
+            switch (command)
+            {
+                case "Screenshot":
+                    byte[] sendBytes = CaptureScreen();
+                    List<byte[]> arrayChunks = sendBytes.Chunk(111).ToList();
+
+                    for (int i = 0; i < arrayChunks.Count; i++)
+                    {
+                        var chunk = arrayChunks[i];
+                        await client.SendAsync(chunk, chunk.Length, clientEP);
+                    }
+                    var loadEndedMessage = Encoding.Default.GetBytes("Transmission Complete");
+                    client.Send(loadEndedMessage, clientEP);
+                    break;
+            }
+        }
+    }
+}
+
+
+byte[] CaptureScreen()
+{
+
+    Bitmap bm;
+    using (MemoryStream ms = new MemoryStream())
+    {
+        using (bm = new Bitmap(1920, 1080))
+        {
+            using (Graphics g = Graphics.FromImage(bm))
+            {
+                g.CopyFromScreen(0, 0, 0, 0, bm.Size);
+            }
+            bm.Save(ms, ImageFormat.Png);
+        }
+        return ms.ToArray();
+    }
+}
+
